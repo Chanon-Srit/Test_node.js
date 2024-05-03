b10504fa53bf7fbfc52f85329aa151514b441d78
Chanon Srit <chanon.srit@dome.tu.ac.th>
2024-05-03 17:04:27 +0700

    connect to mongodb and work with dotenv

--- src/app.js
+++ src/app.js
@@ -1,3 +1,69 @@
-const { v4: uuidv4} = require('uuid');
+const express = require('express');
+const mongoose = require('mongoose');
+const Customer = require('./models/customers');
 
-console.log(uuidv4());
+const app = express();
+mongoose.set('strictQuery', false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
+
+// const customer = [
+//     {
+//         name:"Chanon Sritulakan",
+//         age: "22"
+//     },
+//     {
+//         name:"Wanwisa Channak",
+//         age: "21"
+//     }
+// ];
+
+const customer = new Customer({
+    name: 'Chanon Sritulakan',
+    age: '22'
+});
+
+app.get('/',(req,res)=>{
+    res.send(customer);
+});
+
+app.get('/api/customer',(req,res)=>{
+    res.send({"data":customer});
+});
+
+app.get('/api/customer/name',(req,res)=>{
+    res.send({"data":customer.name});
+});
+
+app.post('/',(req,res)=>{
+    res.send('post request');
+});
+
+app.post('/api/customer',(req,res)=>{
+    console.log(res.body);
+    res.send(res.body);
+});
+
+
+
+const start = async() => {
+    try{
+        await mongoose.connect(CONNECTION);
+        
+        app.listen(PORT, () => {
+            console.log('App listening on port '+PORT)
+        });
+    } catch(e){
+        console.log(e.message)
+    }
+};
+
+start();
